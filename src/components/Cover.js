36b386047d85648327eb5336b7edd39151756399
nuan <nuangrobbelaar@icloud.com>
2023-07-16 19:35:32 +0200

    stuff

---
+++ src/components/Cover.js
@@ -0,0 +1,7 @@
+import "../style/style-main.css";
+
+import Container from "./ui/Container";
+
+export default function Cover(props) {
+	return <Container index={0} positions={{}} name="cover"></Container>;
+}
