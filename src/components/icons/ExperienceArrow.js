bbfdd4878740b2bfcb5e11ec1202ba52aa5d185d
nuan <nuangrobbelaar@icloud.com>
2023-07-18 22:21:27 +0200

    more animations

--- src/components/icons/ExperienceArrow.js
+++ src/components/icons/ExperienceArrow.js
@@ -2,15 +2,14 @@ import "../../style/styles.css";
 
 const ExperienceArrow = (props) => {
 	return (
-		<div id={props.id} className={props.className}>
+		<div className={props.className}>
 			<svg
-				// width="171"
-				// height="77"
 				viewBox="0 -5 342 165"
 				fill="none"
 				xmlns="http://www.w3.org/2000/svg"
 			>
 				<path
+					id="experience-arrow-path"
 					d="M2 48C18 89 71.8 166.8 159 150C246.2 133.2 297.333 63.6667 312 31C303 26.3333 283.8 17.4 279 19C274.2 20.6 309 8.33333 327 2L340 48C335.983 43 325.96 33.6 318 36"
 					stroke="black"
 					stroke-width="7"
