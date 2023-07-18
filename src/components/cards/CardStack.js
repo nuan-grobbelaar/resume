eb954841eab2080cd12cac814f41108955510b38
nuan <nuangrobbelaar@icloud.com>
2023-07-18 21:49:58 +0200

    small change to stack

--- src/components/cards/CardStack.js
+++ src/components/cards/CardStack.js
@@ -14,7 +14,7 @@ const to = (i) => ({
 	rot: 0,
 	delay: i * 100,
 });
-const from = (_i) => ({ x: -50, rot: 0, scale: 1, y: -200 });
+const from = (_i) => ({ x: -800, rot: 0, scale: 1, y: -800 });
 const trans = (r, s) =>
 	`rotateX(30deg) rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`;
 
